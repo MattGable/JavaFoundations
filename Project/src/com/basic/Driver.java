0229b3c5a3f4ca361655653775ce36d5f1ff7302
Matt <matthewgable@gmail.com>
2017-02-16 00:43:35 -0500

    Adding JavaFoundation code

---
+++ Project/src/com/basic/Driver.java
@@ -0,0 +1,15 @@
+package com.basic;
+
+public class Driver {
+
+	public static void main (String [] args) {
+		
+		BasicOperations.PrintHello();
+		BasicOperations.GetUserInput();
+		BasicOperations.HandleInputErrors();
+		
+		FlowControl.Iteration();
+		FlowControl.Recursion();
+	}
+
+}
---
+++ Project/src/com/basic/FlowControl.java
@@ -0,0 +1,29 @@
+package com.basic;
+
+public class FlowControl {
+
+	public static void Iteration() {
+		
+		//A loop that prints its counter every iteration.
+		for (int i = 0; i < 10; i++)
+		{
+			System.out.println("Iterator i is: " + i);
+		}
+	}
+	
+	public static void Recursion() {
+		
+		//This Recursion method uses a simple factorial calculation.
+		//Returns the factorial of a value, 10 in this case;
+		System.out.println("The factorial of 10 is: " + Factorial(10));
+	}
+	
+	private static int Factorial (int i) {
+		if (i == 0) {
+			return 1;
+		}
+		else {
+			return i * Factorial(i - 1);
+		}
+	}
+}
