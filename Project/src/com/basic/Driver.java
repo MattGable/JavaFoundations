0722df8a7db49e4b79adde67d7ad4486c39c9f77
Matt <matthewgable@gmail.com>
2017-02-17 15:45:30 -0500

    Adding samples for inheritance and polymorphism

--- Project/src/com/basic/Driver.java
+++ Project/src/com/basic/Driver.java
@@ -1,5 +1,8 @@
 package com.basic;
 
+import com.basic.ObjectOriented.ChildClass;
+import com.basic.ObjectOriented.ParentClass;
+
 public class Driver {
 
 	public static void main (String [] args) {
@@ -9,8 +12,37 @@ public class Driver {
 		BasicOperations.handleInputErrors();
 		
 		FlowControl.ifElse();
+		FlowControl.switchMethod();
 		FlowControl.iteration();
 		FlowControl.recursion();
+		
+		//Inheritance
+		
+		//Creating an instance of a ChildClass (from a nesting class).
+		ChildClass myChildClass = new ObjectOriented().new ChildClass();
+		
+		//This overrides the same method from a parent.
+		myChildClass.myPrint();
+		//This happens only in a ChildClass.
+		myChildClass.childPrint();
+		//This is inherited from a ParentClass.
+		myChildClass.parentPrint();
+		
+		//Polymorphism
+		
+		//A child can still act like a parent.
+		//If we cast child to parent, however, we still have child versions of parent methods, since its
+		//original reference is for a ChildClass.
+
+		ParentClass childToParent = (ParentClass)myChildClass;
+		//ChildClass version.
+		childToParent.myPrint();
+		//ParentClass version (not implemented in ChildClass).
+		childToParent.parentPrint();
+		
+		//However, we lose access to ChildClass-only methods!
+		//This does not work without casting it to ChildClass.
+		//childToParent.childPrint();
 	}
 
 }
--- Project/src/com/basic/ObjectOriented.java
+++ Project/src/com/basic/ObjectOriented.java
@@ -2,7 +2,30 @@ package com.basic;
 
 public class ObjectOriented {
 
-	public interface MyInterface () {
+	public class ParentClass {
 		
+		//Abstract classes can also have their own fields.
+		int parentValue = 10;
+		
+		public void myPrint () {
+			System.out.println("This is printed as a ParentClass.");
+		}
+		
+		public void parentPrint () {
+			System.out.println("Printing as a ParentClass again.");
+		}
+		
+	}
+	
+	public class ChildClass extends ParentClass {
+		
+		@Override
+		public void myPrint () {
+			System.out.println("I am overriding behavior from my parent.");
+		}
+		
+		public void childPrint () {
+			System.out.println("Printing from ChildClass!");
+		}
 	}
 }
